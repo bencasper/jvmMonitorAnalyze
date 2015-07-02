229cd324bf53e6c5d773c8ef7c75213fc1ca8384
ben <zsydn007@gmail.com>
2015-07-02 18:59:10 +0800

    monitor server upload

---
+++ monitor_server_upload.py
@@ -0,0 +1,25 @@
+from subprocess import Popen, PIPE
+from re import split
+
+__author__ = 'ben'
+uploadThreshold = 10  # set linux upload threshold to 10
+
+
+class UploadMonitor:
+    def __init__(self):
+        self.command = 'uptime'
+
+    def monitor_upload(self):
+        sub_proc = Popen([self.command], shell=False, stdout=PIPE)
+        line = sub_proc.stdout.readline()
+        # The separator for splitting is 'variable number of spaces'
+        print line
+        proc_info = split("load averages:", line)
+        uptime = proc_info[1]
+        nearly_uptime = split(" ", uptime)[1]
+        print nearly_uptime
+        if nearly_uptime > uploadThreshold:
+            """ do the analyze and send email """
+
+
+UploadMonitor().monitor_upload()
