134f0cd40f2fd160d7f9f150ec2325a5fad1faa7
ben <zsydn007@gmail.com>
2015-07-03 17:06:11 +0800

    get the abs path of the conf file

--- conf_parse.py
+++ conf_parse.py
@@ -1,7 +1,8 @@
+from inspect import getsourcefile
 import json
 import logging
 import os
-from os.path import join
+from os.path import join, abspath, split
 
 __author__ = 'ben'
 
@@ -11,8 +12,12 @@ logging.basicConfig()
 
 
 def parse_conf():
+    sourcefile = getsourcefile(lambda:0)
+    logger.info('source file is %s;',sourcefile)
+    fd = split(sourcefile)[0]
+    logger.info('source file path is %s;',fd)
     # read from file to get mail info
-    conf_file = join(os.getcwd(), 'conf', 'monitor_configure.conf')
+    conf_file = join(fd, 'conf', 'monitor_configure.conf')
     logger.info('conf_file is %s', conf_file)
     with open(conf_file, 'r') as conf:
         conf_json = json.load(conf, 'utf-8')
@@ -26,3 +31,7 @@ def mk_log_dir():
         pass
     else:
         os.mkdir(log_path)
+
+
+if __name__ == "__main__":
+    parse_conf()
