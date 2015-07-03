90a646c977816e8232b1a61a6073a982175f55bd
ben <zsydn007@gmail.com>
2015-07-03 15:57:15 +0800

    create monitor dir is not exist

--- jvmMonitor.py
+++ jvmMonitor.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+import os
+
 from email_send import EmailSend
 from monitor_http_api import APIMonitor
 from monitor_server_uptime import UptimeMonitor
@@ -6,6 +8,13 @@ from monitor_server_uptime import UptimeMonitor
 __author__ = 'ben'
 
 if __name__  == "__main__":
+    log_path = '/letv/logs/monitor'
+    log_file = 'jvmdump.log'
+    if os.path.isdir(log_path):
+        pass
+    else:
+        os.mkdir(log_path)
+
     APIMonitor().do_monitor()
     UptimeMonitor().do_monitor()
     EmailSend().write_email()
