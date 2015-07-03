20b5a7f4829407e3df430473cb8a32c7b9f4f185
ben <zsydn007@gmail.com>
2015-07-03 14:45:36 +0800

    main exec as a shell

--- jvmMonitor.py
+++ jvmMonitor.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python
 from email_send import EmailSend
 from monitor_http_api import APIMonitor
 from monitor_server_uptime import UptimeMonitor
 
 __author__ = 'ben'
 
-APIMonitor().do_monitor()
-UptimeMonitor().do_monitor()
-EmailSend.write_email()
+if __name__  == "__main__":
+    APIMonitor().do_monitor()
+    UptimeMonitor().do_monitor()
+    EmailSend.write_email()
 
