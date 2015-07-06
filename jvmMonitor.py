595002e4a6f66fd2d7d99761305a9c71c31e987e
ben <zsydn007@gmail.com>
2015-07-06 11:07:30 +0800

    truncate log file before each executing

--- jvmMonitor.py
+++ jvmMonitor.py
@@ -3,13 +3,14 @@ import os
 
 from email_send import EmailSend
 from exec_analyze_shell import exec_monitor_shell
+from file_utils import get_log_file
 from monitor_http_api import APIMonitor
 from monitor_server_uptime import UptimeMonitor
 
 __author__ = 'ben'
 
 if __name__  == "__main__":
-    os.system('truncate -s 0 test.txt')
+    os.system('truncate -s 0 ' + get_log_file())
     API_diagnosis = APIMonitor().do_monitor()
     Uptime_diagnosis = UptimeMonitor().do_monitor()
     if API_diagnosis or Uptime_diagnosis:
