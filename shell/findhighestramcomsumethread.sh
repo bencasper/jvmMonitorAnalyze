9f1a46036ce6f1cac7d8ae9b8a7800815652b2f7
ben <zsydn007@gmail.com>
2015-08-17 15:49:32 +0800

    optimize jstack command

--- shell/findhighestramcomsumethread.sh
+++ shell/findhighestramcomsumethread.sh
@@ -1,14 +1,21 @@
-#!//bin/sh
+#!/bin/bash
 LOG_FILE='/letv/logs/monitor/jvmdump.log'
-TIDDS=$(top -a -n5 -H | grep -m5 java | perl -pe 's/\e\[?.*?[\@-~] ?//g' | cut -f1 -d' ')
-tidd_arr=($TIDDS)
-echo 'highest ram consume threads info:' $(date) >> $LOG_FILE
-for TIDD in "${tidd_arr[@]}"
+JSTACK_PATH='/letv/logs/monitor/'
+RESINPIDS=$(jps | grep Resin | cut -f1 -d ' ')
+resinpid_arr=($RESINPIDS)
+for RESINPID in "${resinpid_arr[@]}"
 do
+    TIDDS=$(top -a -n5 -H -p ${RESINPID} | grep -m5 java | perl -pe 's/\e\[?.*?[\@-~] ?//g' | cut -f1 -d' ')
+    tidd_arr=($TIDDS)
+    echo 'highest ram consume threads info:'$(date) >> $LOG_FILE
+    for TIDD in "${tidd_arr[@]}"
+    do
         echo 'TIDD' $TIDD >> $LOG_FILE
         TIDH=$(printf "%x" $TIDD)
         echo 'TIDH' $TIDH >> $LOG_FILE
-        PID=$(ps -aefL | grep java | grep -m1 ${TIDD} | awk '{print $3}')
-        echo 'PID' $PID >> $LOG_FILE
-        jstack $PID | grep -A500 $TIDH | grep -m1 "^$" -B 500 >> $LOG_FILE
+        #PID=$(ps -aefL | grep java | grep -m1 ${TIDD} | awk '{print $3}')
+        echo 'PID' ${RESINPID} >> $LOG_FILE
+        jstack ${RESINPID} | grep -A500 $TIDH | grep -m1 "^$" -B 500 >> $LOG_FILE
+    done
 done
+
